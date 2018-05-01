21e4be684f83be5b2b08a7dc5628f49f68815fef
rixlai <rixktl@gmail.com>
2018-04-30 19:18:31 -0700

    Card.IO example

--- app/src/main/java/gydes/gyde/MainActivity.java
+++ app/src/main/java/gydes/gyde/MainActivity.java
@@ -2,12 +2,25 @@ package gydes.gyde;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button cardIOButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cardIOButton = (Button) findViewById(R.id.cardIOPage);
+        cardIOButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, CardIOExample.class);
+                startActivity(intent);
+            }
+        });
     }
 }

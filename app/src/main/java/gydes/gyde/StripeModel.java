f584a23b1b46af67a17a8bc1bfb5c881c1998b07
rixlai <rixktl@gmail.com>
2018-05-10 04:16:59 -0700

    Added firebase (read&write) for card

--- app/src/main/java/gydes/gyde/StripeModel.java
+++ app/src/main/java/gydes/gyde/StripeModel.java
@@ -1,17 +1,27 @@
 package gydes.gyde;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
 import com.stripe.android.Stripe;
 import com.stripe.android.TokenCallback;
 import com.stripe.android.model.Token;
 import com.stripe.android.model.Card;
 import com.stripe.model.Charge;
 import com.stripe.model.Customer;
-import com.stripe.model.Source;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import android.content.Context;
-import android.widget.Toast;
+import android.util.Log;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static java.security.AccessController.getContext;
@@ -24,12 +34,41 @@ public class StripeModel {
     private final static String STRIPE_PRIVATE_API_KEY = "sk_test_ZbL5ejRj63bOCU1mUbVZBdiL";
     private final static String STRIPE_PUBLIC_API_KEY = "pk_test_6mUSZUJf0upFQzlocgvyQknF";
     private Stripe stripe;
+    private FirebaseUser currentUser;
+    private String dataPath;
+    private DatabaseReference databaseRef;
+    private List<Object> cards;
 
     public StripeModel(Context context) {
         stripe = new Stripe(context, STRIPE_PUBLIC_API_KEY);
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        // Assume path already exist for this user
+        //dataPath = "Traveller/" + currentUser.getUid();
+        dataPath = "Traveller" + "/" + "No_UID_Yet_User";
+        databaseRef = FirebaseDatabase.getInstance().getReference(dataPath);
+        cards = new ArrayList<Object>();
+
+        databaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                System.out.println("Cards:");
+                System.out.println(cards.toString());
+                cards.clear(); // clear local
+                DataSnapshot cardsSnapshot = dataSnapshot.child("cards");
+                for(DataSnapshot child: cardsSnapshot.getChildren()) {
+                    Object obj = child.getValue();
+                    cards.add(obj); // Append all cards
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // TODO: handle error
+            }
+        });
     }
 
-    public boolean addCard(SimpleCard card) {
+    public boolean addCard(final SimpleCard card) {
         Card newCard = new Card(
                 card.getCardNumber(),
                 card.getExpiryMonth(),
@@ -46,9 +85,15 @@ public class StripeModel {
                 new TokenCallback() {
                     public void onSuccess(Token token) {
                         // Received token
-                        System.out.println("Received token");
-                        // TODO: Save token to firebase
-                        charge(token); // For testing
+                        SimpleCard cardWithToken = new SimpleCard();
+                        cardWithToken.setLast4(card.getLast4());
+                        cardWithToken.setCVV(card.getCVV());
+                        cardWithToken.setExpiryMonth(card.getExpiryMonth());
+                        cardWithToken.setExpiryYear(card.getExpiryYear());
+                        cardWithToken.setStripeToken(token.getId());
+                        cards.add(cardWithToken);
+                        databaseRef.child("cards").setValue(cards); // Push to firebase
+                        // charge(token); // For testing
                     }
                     public void onError(Exception error) {
                         System.out.println("Create token failed");

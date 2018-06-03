b9e338a9298385561aae7d2defe55807b479121e
Andy Ruan <a.ruan2016@gmail.com>
2018-06-03 01:28:09 -0700

    .

--- app/src/main/java/gydes/gyde/controllers/SearchResults.java
+++ app/src/main/java/gydes/gyde/controllers/SearchResults.java
@@ -208,6 +208,8 @@ public class SearchResults extends AppCompatActivity {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(SearchResults.this);
         View view = getLayoutInflater().inflate(R.layout.tour_filter_dialog, null);
+
+
         builder.setView(view);
 
         AlertDialog dialog = builder.create();

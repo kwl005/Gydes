b4427a3027904cd25fd9e8b2df55ca705139137c
Kelvin Lui <kelflo1120@gmail.com>
2018-06-03 18:40:17 -0700

    saved

--- app/src/main/java/gydes/gyde/controllers/addTour/AddTourConfirmMethod.java
+++ app/src/main/java/gydes/gyde/controllers/addTour/AddTourConfirmMethod.java
@@ -1,27 +1,18 @@
 package gydes.gyde.controllers.addTour;
 
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
-
 import com.mikepenz.materialdrawer.Drawer;
-import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
-import com.wdullaer.materialdatetimepicker.time.Timepoint;
-
-import java.security.Key;
-import java.util.Calendar;
-
 import gydes.gyde.R;
 import gydes.gyde.controllers.KeyboardManager;
 import gydes.gyde.controllers.NavigationDrawerBuilder;
@@ -30,11 +21,12 @@ import gydes.gyde.controllers.NavigationDrawerBuilder;
  * Created by kelvinlui1 on 6/3/18.
  */
 
-public class AddTourConfirmDuration extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
+public class AddTourConfirmMethod extends AppCompatActivity {
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_tour_confirm_duration);
+        setContentView(R.layout.activity_add_tour_confirm_method);
 
         // Setup navigation drawer, action bar and status bar
         final Drawer result = NavigationDrawerBuilder.build(this, savedInstanceState);
@@ -54,30 +46,32 @@ public class AddTourConfirmDuration extends AppCompatActivity implements TimePic
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.setStatusBarColor(ContextCompat.getColor(this, R.color.gydeYellow));
 
-        // Display time picker as soon as the screen loads
-        TimePickerDialog tpd = TimePickerDialog.newInstance(AddTourConfirmDuration.this, false);
-        tpd.setCancelText("BACK");
-        tpd.setMaxTime(11, 59, 59);
-        tpd.enableMinutes(false);
-        tpd.setVersion(TimePickerDialog.Version.VERSION_2);
-        tpd.setTitle("Select duration of your tour");
-        tpd.setInitialSelection(2, 0, 0);
-        tpd.setOnCancelListener(new DialogInterface.OnCancelListener() {
-            @Override
-            public void onCancel(DialogInterface dialogInterface) {
-                finish();
-                KeyboardManager.showKeyboard(AddTourConfirmDuration.this);
-            }
-        });
-        tpd.show(getFragmentManager(), "timePickerDialog");
-    }
+        // Show dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final CharSequence[] methods = {"Walking", "Driving", "Back"};
+        builder.setTitle("Select your travel method")
+                .setItems(methods, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Bundle bundle = getIntent().getExtras();
+                        switch(i) {
+                            case 0:
+                                bundle.putString("walking", "true");
+                                break;
+                            case 1:
+                                bundle.putString("walking", "false");
+                                break;
+                            case 2:
+                                finish();
+                                break;
+                            default:
+                                throw new IllegalStateException("Selected method must be either index 0, 1 or 2.");
+                        }
 
-    @Override
-    public void onTimeSet(TimePickerDialog view, int hourOfDay, int minute, int second) {
-        Bundle bundle = getIntent().getExtras();
-        bundle.putInt("duration", hourOfDay);
-        Intent intent = new Intent(this, AddTourConfirmMethod.class);
-        intent.putExtras(bundle);
-        startActivity(intent);
+                        // TODO
+                    }
+                });
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
